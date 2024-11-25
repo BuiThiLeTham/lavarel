c9feb8f1c61a429482e189cc353d929e5aa5ebcf
BuiThiLeTham <thambtl.23it@vku.udn.vn>
2024-11-25 23:53:25 +0700

    bai1-p2

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,16 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','App\Http\Controllers\HomeController@index'); 
 
 Route::get('/trang-chu','App\Http\Controllers\HomeController@index');
+Route::get('/product', function () {
+    return view('pages.home');
+   });
+   Route::get('/product', function () {
+    return view('pages.product');
+   });
+   Route::get('/news', function () {
+    return view('pages.news');
+   });
+   Route::get('/contact', function () {
+    return view('pages.contact');
+   });
 
